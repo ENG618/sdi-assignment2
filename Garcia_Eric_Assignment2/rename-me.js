f728293bf545dc4280c0daa91cb43b3107afa02a
eng618 <eng618@fullsail.edu>
2013-03-14 18:46:46 -0400

    Starting Project 2

--- Garcia_Eric_Assignment2/rename-me.js
+++ Garcia_Eric_Assignment2/rename-me.js
@@ -1,18 +1,6 @@
 //alert("JavaScript works!");
 
-var tankNames = ["Shark Tank", "Sting Ray Tank", "Dolphin Tank", "Fish Tank"],
-	minutesPerTank = [20, 15, 25, 30],
-	kidNames = ["Braden", "Rhys", "Zoe"];
-var visitTank = function(whatTank) {
-    var tankName = tankNames[whatTank],
-    minutesThisTank = minutesPerTank[tankNumber];
-    console.log(kidNames[0] + " wanted us to visit the " + tankName + " for " + minutesThisTank + " minutes. So I set the timer on my watch!");
-    for (var minutes = 0; minutes < minutesThisTank; minutes += 5) {
-            var minutesRemain = minutesThisTank - minutes;
-            console.log("We have " + minutesRemain + " minutes left. " + minutes + " minutes have past.");
-    }
-    console.log("We finished with the " + tankName + ".");
-};
-for (var tankNumber = 0; tankNumber < tankNames.length; tankNumber++) {
-    visitTank(tankNumber); 
-};
+//Eric Garcia
+//SDI 1303
+//Project 2
+//
